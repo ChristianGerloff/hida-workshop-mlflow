b696cb486d30a26a1d51c36f11bef07d146fba73
ChristianGerloff <christian.gerloff@rwth-aachen.de>
2021-07-09 09:57:42 +0200

    add multistage pipeline #2

---
+++ mlflow_projects/start.py
@@ -0,0 +1,28 @@
+import os
+import mlflow
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URL'))
+mlflow.set_experiment(os.getenv('MLFLOW_EXPERIMENT'))
+
+mlflow.projects.run(
+    '.',  # here you could at the URL to your github/ gitlab repo
+    backend='local', 
+    synchronous=True,
+    entry_point='main',
+    parameters={'bucket': os.getenv('BUCKET_NAME'),
+                'dataset': 'asd',
+                'alpha': 0.9,
+                'n_components': 20})
+
+mlflow.projects.run(
+    '.',  # here you could at the URL to your github/ gitlab repo
+    backend='local', 
+    synchronous=True,
+    entry_point='main',
+    parameters={'bucket': os.getenv('BUCKET_NAME'),
+                'dataset': 'asd',
+                'alpha': 0.9,
+                'n_components': 5})
